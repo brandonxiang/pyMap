84106c01a96bf6c24377621bb75e3b94c038b9dc
brandonxiang <1542453460@qq.com>
2016-05-26 10:03:01 +0800

    requirements.txt readme

--- pyMap.py
+++ pyMap.py
@@ -5,22 +5,28 @@ import requests
 from PIL import Image
 
 
-def process_latlng(north, west, south, east, zoom):
+def process_latlng(north, west, south, east, zoom, output='output/mosaic.png'):
+    """
+    download and mosaic by latlng
+    """
     left, top = latlng2tilenum(north, west, zoom)
     right, bottom = latlng2tilenum(south, east, zoom)
-    process_tilenum(left, right, top, bottom, zoom)
+    process_tilenum(left, right, top, bottom, zoom, output)
 
 
-def process_tilenum(left, right, top, bottom, zoom):
+def process_tilenum(left, right, top, bottom, zoom, output):
+    """
+    download and mosaic by tile number 
+    """
     for x in range(left, right + 1):
         for y in range(top, bottom + 1):
             path = './tiles/%i/%i/%i' % (zoom, x, y)
             if not os.path.exists(path):
-                download(x, y, zoom)
-    mosaic(left, right, top, bottom, zoom)
+                _download(x, y, zoom)
+    _mosaic(left, right, top, bottom, zoom, output)
 
 
-def download(x, y, z):
+def _download(x, y, z):
     url = "http://webst02.is.autonavi.com/appmaptile?style=6&x=%i&y=%i&z=%i" % (x, y, z)
     r = requests.get(url, stream=True)
     path = './tiles/%i/%i' % (z, x)
@@ -32,9 +38,10 @@ def download(x, y, z):
                 f.write(chunk)
                 f.flush()
         f.close()
+    print('Downloaded', x, y, z)
 
 
-def mosaic(left, right, top, bottom, zoom):
+def _mosaic(left, right, top, bottom, zoom, output):
     size_x = (right - left + 1) * 256
     size_y = (bottom - top + 1) * 256
     output_im = Image.new("RGB", (size_x, size_y))
@@ -44,7 +51,11 @@ def mosaic(left, right, top, bottom, zoom):
             path = './tiles/%i/%i/%i' % (zoom, x, y)
             target_im = Image.open(path)
             output_im.paste(target_im, (256 * (x - left), 256 * (y - top)))
-    output_im.save('mosaic.png')
+    output_path = os.path.split(output)
+    if len(output_path) > 1 and len(output_path) != 0:
+        if not os.path.isdir(output_path[0]):
+            os.makedirs(output_path[0])
+    output_im.save(output)
 
 
 def latlng2tilenum(lat_deg, lng_deg, zoom):
@@ -60,14 +71,14 @@ def latlng2tilenum(lat_deg, lng_deg, zoom):
 
 def test():
     process_latlng(22.4566710000, 113.8899620000, 22.3455760000, 114.2126860000, 13)
-    
+
+
 def cml():
-    if(len(sys.argv)!=6):
-        print('input 5 parameter northeast latitude,northeast longitude, southeast latitude, southeast longitude')
+    if not len(sys.argv) in [6, 7]:
+        print('input 6 parameter northeast latitude,northeast longitude, southeast latitude, southeast longitude,zoom, output file')
         return
-    # print(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
-    process_latlng(float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), int(sys.argv[5]))
+    process_latlng(float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), int(sys.argv[5]), str(sys.argv[6]))
 
 if __name__ == '__main__':
-    # sys.exit(int(test() or 0))
+    # test()
     cml()
