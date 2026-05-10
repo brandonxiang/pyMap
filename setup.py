5bee59d7009ef3b496f69f97c44c9a71659c89b2
brandonxiang <brandonxiang@qq.com>
2026-05-11 00:30:43 +0800

    chore: prepare pymap for pypi publishing

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     description="Download raster map tiles and stitch them into PNG mosaics.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
+    author="Brandon Xiang",
+    url="https://github.com/brandonxiang/pyMap",
     py_modules=["pyMap"],
     install_requires=[
         "requests>=2.10.0",
@@ -21,4 +23,14 @@ setup(
     },
     python_requires=">=3.5",
     license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Topic :: Utilities",
+    ],
 )
